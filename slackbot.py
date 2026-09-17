f0c38ef3ca0d7b209b993417e6e8425d84f7bb5a
CatPowerz <121599451+CatPowerz@users.noreply.github.com>
2026-09-17 21:05:45 +0200

    Refactor Slackbot commands to use ack and respond
    Final commit to add .env variables correctly to add your own app and bot keys.

--- slackbot.py
+++ slackbot.py
@@ -1,7 +1,12 @@
 import os
 import random
+from pathlib import Path
+from dotenv import load_dotenv
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
+
+env_path = Path(__file__).resolve().parent / ".env"
+load_dotenv(dotenv_path=env_path)
 
 app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
 
@@ -11,14 +16,12 @@ motivationalquotes = [
     "> \"Think about the cake.\"",
 ]
 
-
 boostquotes = [
     "> \"It's not a bug, it is a feature.\"",
     "> \"First you have to solve the problem, then you write the code.\"",
     "> \"The idea is half of the project. So is the name.\"",
 ]
 
-# 3. Film / Popcultuur quote
 focusquotes = [
     "> \"The best way to focus is pausing for five minutes.\"",
     "> \"Quality > Quantity\"",
@@ -26,17 +29,19 @@ focusquotes = [
 ]
 
 @app.command("/cat-quote")
-def send_motivational_quote(message, say):
-    say(random.choice(motivationalquotes))
+def send_motivational_quote(ack, respond):
+    ack()
+    respond(random.choice(motivationalquotes))
 
 @app.command("/cat-focus")
-def send_focus_quote(message, say):
-    say(random.choice(focusquotes))
-
+def send_focus_quote(ack, respond):
+    ack()
+    respond(random.choice(focusquotes))
 
 @app.command("/cat-boost")
-def send_boost_quote(message, say):
-    say(random.choice(boostquotes))
+def send_boost_quote(ack, respond):
+    ack()
+    respond(random.choice(boostquotes))
 
 if __name__ == "__main__":
     handler = SocketModeHandler(app, os.environ.get("SLACK_APP_TOKEN"))
